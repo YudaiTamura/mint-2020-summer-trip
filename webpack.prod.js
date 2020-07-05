90a6a39fc80d6f96323701570d7cc1c7217b9fa1
YudaiTamura <y-tam@live.jp>
2020-07-05 21:03:53 +0900

    雛形となるファイル一式を追加

---
+++ webpack.prod.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: [path.resolve(__dirname, "./src/index.tsx")],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+
+  devtool: "none",
+};
