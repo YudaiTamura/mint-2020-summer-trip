2d0cad9d74bfd066bc5a00fc8aee9e87cd1b8141
YudaiTamura <y-tam@live.jp>
2020-07-11 17:49:29 +0900

    index.tsxとcomponents/App.tsxを統合

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Reset } from "styled-reset";
+import styled from "styled-components";
 
-import App from "./components/App";
+import TopPage from "./components/TopPage";
+import Footer from "./components/Footer";
+import Variables from "./components/Variables";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const MainContents = styled.main`
+  /* 画面の高さ - footerの高さ */
+  min-height: calc(100vh - 40px);
+  width: 100vw;
+  background-color: ${Variables.COLOR.BASE};
+`;
+
+ReactDOM.render(
+  <>
+    <Reset />
+    <MainContents>
+      <TopPage />
+    </MainContents>
+    <Footer />
+  </>,
+  document.getElementById("root")
+);
