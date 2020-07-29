6667f03feee362a1fb5e97d162f3a6ec80d1debf
YudaiTamura <y-tam@live.jp>
2020-07-29 23:50:11 +0900

    index.jsを今まで作ってきたindex.tsxに差し替え

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-/**
- * @return {void}
- */
-export default function Home() {
-  return <div>Hello world!!!!!!!</div>;
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import TopPage from '../components/TopPage';
+import Himejijo from '../components/Himejijo';
+import Footer from '../components/Footer';
+import Variables from '../components/Variables';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    border: none;
+    font-family: serif;
+  }
+  button:focus {
+    outline:0;
+  }
+`;
+
+const MainContents = styled.main`
+  min-height: calc(100vh - ${Variables.FOOTER_HEIGHT});
+  width: 100vw;
+  background-color: ${Variables.COLOR.BASE};
+`;
+
+const Index: React.FC = () => {
+  return (
+    <>
+      <Reset />
+      <GlobalStyle />
+      <MainContents>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={TopPage}></Route>
+            <Route exact path="/himejijo" component={Himejijo}></Route>
+          </Switch>
+        </BrowserRouter>
+        <Footer />
+      </MainContents>
+    </>
+  );
+};
+
+export default Index;
